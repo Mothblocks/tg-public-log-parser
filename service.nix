5a15990355db89b6907d059913f636e0b1521be4
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-09-27 16:34:03 -0400

    New approach

--- service.nix
+++ service.nix
@@ -8,62 +8,28 @@ inputs@{
 let
   package = import ./package.nix inputs;
   config-format = pkgs.formats.toml { };
-  cfg = config.services.tg-public-log-parser;
-  service-instances = lib.attrNames cfg;
   package-wrapper = instance-name: pkgs.writeShellScriptBin "tg-public-log-parser-wrapper" ''
     cd /etc/tg-public-log-parser.d/${instance-name}
     exec ${package}/bin/tg-public-log-parser
   '';
-  built-service-implementation = lib.genAttrs (service-instances) (instance-name: lib.mkIf cfg."${instance-name}".enable {
-    environment.etc = {
-      "tg-public-log-parser.d/${instance-name}/config.toml" = {
-        source = pkgs.formats.toml.generate "config" cfg."${instance-name}".config;
-        mode = "0444";
-      };
-    };
-
-    systemd.services."tg-public-log-parser-${instance-name}" = {
-      description = "tg-public-log-parser-${instance-name}";
-      serviceConfig = {
-        Type = "simple";
-        DynamicUser = true;
-        SupplementaryGroups = cfg."${instance-name}".supplementary-groups;
-        ExecStart = "${(package-wrapper instance-name)}/bin/tg-public-log-parser-wrapper";
-        KillMode = "control-group";
-        KillSignal = "KILL";
-        Environment = "RUST_LOG=info";
-      };
-      wantedBy = [ "multi-user.target" ];
-      after = ["network.target"];
-    };
-  });
 in
 {
-  options.services.tg-public-log-parser = {
-    instances = lib.mkOption {
-      description = ''
-        Configure instances of the tg-public-log-parser.
-      '';
-      type = lib.types.attrsOf (
-        lib.types.submodule (
-          { instance-name, ... }:
-          {
-            options = {
-              enable = lib.mkEnableOption "tg-public-log-parser for ${instance-name}";
-              supplementary-groups = lib.mkOption {
-                type = lib.types.str;
-                default = "";
-                description = ''
-                  Extra groups to give the service access to.
-                '';
-              };
-              config = lib.mkOption {
-                inherit (config-format) type;
-                default = { };
-                description = lib.mdDoc ''
-                  Configuration included in `config.toml`.
-                '';
-              };
+  options.services.tg-public-log-parser = lib.mkOption {
+    description = ''
+      Configure instances of the tg-public-log-parser.
+    '';
+    type = lib.types.attrsOf (
+      lib.types.submodule (
+        { instance-name, ... }:
+        {
+          options = {
+            enable = lib.mkEnableOption "tg-public-log-parser for ${instance-name}";
+            supplementary-groups = lib.mkOption {
+              type = lib.types.str;
+              default = "";
+              description = ''
+                Extra groups to give the service access to.
+              '';
             };
             config = lib.mkOption {
               inherit (config-format) type;
@@ -72,11 +38,33 @@ in
                 Configuration included in `config.toml`.
               '';
             };
-          }
-        )
-      );
-    };
+          };
+        }
+      )
+    );
   };
 
-  config = lib.mkIf cfg.enable built-service-implementation;
+  config = {
+    environment.etc = lib.mapAttrs (instance-name: instance-config: lib.mkIf cfg."${instance-name}".enable {
+      "tg-public-log-parser.d/${instance-name}/config.toml" = {
+        source = pkgs.formats.toml.generate "config" instance-config.config;
+        mode = "0444";
+      };
+    }) config.services.tg-public-log-parser;
+
+    systemd.services = lib.mapAttrs (instance-name: instance-config: lib.mkIf instance-config.enable {
+      description = "tg-public-log-parser-${instance-name}";
+      serviceConfig = {
+        Type = "simple";
+        DynamicUser = true;
+        SupplementaryGroups = instance-config.supplementary-groups;
+        ExecStart = "${(package-wrapper instance-name)}/bin/tg-public-log-parser-wrapper";
+        KillMode = "control-group";
+        KillSignal = "KILL";
+        Environment = "RUST_LOG=info";
+      };
+      wantedBy = [ "multi-user.target" ];
+      after = ["network.target"];
+    }) config.services.tg-public-log-parser;
+  };
 }
