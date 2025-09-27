ef511707b78ea1b2cdb619879b6617ff4c5b21eb
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-09-27 16:51:12 -0400

    Try map attrs like this

--- service.nix
+++ service.nix
@@ -45,15 +45,18 @@ in
   };
 
   config = {
-    environment.etc = lib.mapAttrs (instance-name: instance-config: lib.mkIf instance-config.enable {
-      "tg-public-log-parser.d/${instance-name}/config.toml" = {
-        source = config-format.generate "config" instance-config.config;
-        mode = "0444";
-      };
+    environment.etc = lib.mapAttrs' (instance-name: instance-config: 
+      lib.mkIf instance-config.enable {
+      (lib.nameValuePair "tg-public-log-parser.d/${instance-name}/config.toml" 
+        {
+          source = config-format.generate "config" instance-config.config;
+          mode = "0444";
+        })
     }) config.services.tg-public-log-parser;
 
-    systemd.services = lib.mapAttrs (instance-name: instance-config: lib.mkIf instance-config.enable {
-      "tg-public-log-parser-${instance-name}" = {
+    systemd.services = lib.mapAttrs' (instance-name: instance-config:
+      lib.mkIf instance-config.enable 
+      (lib.nameValuePair "tg-public-log-parser-${instance-name}" {
         description = "tg-public-log-parser-${instance-name}";
         serviceConfig = {
           Type = "simple";
@@ -66,7 +69,6 @@ in
         };
         wantedBy = [ "multi-user.target" ];
         after = ["network.target"];
-      };
-    }) config.services.tg-public-log-parser;
+      })) config.services.tg-public-log-parser;
   };
 }
