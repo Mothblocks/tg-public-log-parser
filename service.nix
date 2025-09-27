8332744d7e9d50f342ab2e5e0104b2d260f09b96
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-09-27 16:56:26 -0400

    I don't see the problem

--- service.nix
+++ service.nix
@@ -47,27 +47,32 @@ in
   config = {
     environment.etc = lib.mapAttrs' (instance-name: instance-config: 
       lib.mkIf instance-config.enable
-      (lib.nameValuePair "tg-public-log-parser.d/${instance-name}/config.toml" 
-        {
+      {
+        name = "tg-public-log-parser.d/${instance-name}/config.toml";
+        value = {
           source = config-format.generate "config" instance-config.config;
           mode = "0444";
-        })) config.services.tg-public-log-parser;
+        };
+      }) config.services.tg-public-log-parser;
 
     systemd.services = lib.mapAttrs' (instance-name: instance-config:
       lib.mkIf instance-config.enable 
-      (lib.nameValuePair "tg-public-log-parser-${instance-name}" {
-        description = "tg-public-log-parser-${instance-name}";
-        serviceConfig = {
-          Type = "simple";
-          DynamicUser = true;
-          SupplementaryGroups = instance-config.supplementary-groups;
-          ExecStart = "${(package-wrapper instance-name)}/bin/tg-public-log-parser-wrapper";
-          KillMode = "control-group";
-          KillSignal = "KILL";
-          Environment = "RUST_LOG=info";
+      {
+        name = "tg-public-log-parser-${instance-name}";
+        value = {
+          description = "tg-public-log-parser-${instance-name}";
+          serviceConfig = {
+            Type = "simple";
+            DynamicUser = true;
+            SupplementaryGroups = instance-config.supplementary-groups;
+            ExecStart = "${(package-wrapper instance-name)}/bin/tg-public-log-parser-wrapper";
+            KillMode = "control-group";
+            KillSignal = "KILL";
+            Environment = "RUST_LOG=info";
+          };
+          wantedBy = [ "multi-user.target" ];
+          after = ["network.target"];
         };
-        wantedBy = [ "multi-user.target" ];
-        after = ["network.target"];
-      })) config.services.tg-public-log-parser;
+      }) config.services.tg-public-log-parser;
   };
 }
