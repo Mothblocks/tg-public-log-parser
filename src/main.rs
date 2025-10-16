e0fddde6e36ef76b3225274a70f9724fd4d5ce0d
Nick Farley <nick@nfreader.net>
2025-10-16 13:34:28 -0400

    Add CORS headers to response

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::sync::Arc;
-
-use axum::Router;
+use axum::{routing::get, Router};
 use eyre::Context;
+use http::Method;
+use std::sync::Arc;
+use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::prelude::*;
 
 mod app_state;
@@ -25,10 +26,22 @@ async fn main() -> eyre::Result<()> {
     tracing::info!("hosting on {}", state.config.address);
 
     let listener = tokio::net::TcpListener::bind(state.config.address).await?;
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+        .allow_headers([
+            http::header::CONTENT_TYPE,
+            http::header::ACCEPT,
+            http::header::AUTHORIZATION,
+        ]);
+
     let app = Router::new()
-        .route("/", axum::routing::get(route::get))
-        .route("/{*path}", axum::routing::get(route::get))
-        .route("/favicon.ico", axum::routing::get(|| async { "" }))
+        .without_v07_checks()
+        .route("/", get(route::get))
+        .route("/favicon.ico", get(|| async { "" }))
+        .route("/*path", get(route::get))
+        .layer(cors)
         .with_state(state);
 
     axum::serve(listener, app.into_make_service()).await?;
